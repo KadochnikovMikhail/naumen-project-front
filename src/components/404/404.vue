ae973a29c327dd56132a2b059a6895c50ad4df7a
Kadochnikov Mikhail <kadochnikov.mihaylo@yandex.ru>
2023-05-10 09:28:57 +0500

    Add 404 page

---
+++ src/components/404/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="err-404">
+    <div class="err-404__block">
+      <p class="err-404__block_num">4</p>
+      <img class="err-404__block_img" src="../../assets/images/Err404.png" alt="404">
+      <p class="err-404__block_num">4</p>
+    </div>
+    <p class="err-404__text">Something wrong</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "404Component"
+}
+</script>
+
+<style src="./404.css" scoped>
+
+</style>
