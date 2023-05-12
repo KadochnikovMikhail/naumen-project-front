94a9b408681c1e1278eabb4f891b05cc67103155
Artem Kiselev <kuzuru.dev@gmail.com>
2023-05-12 14:32:29 +0500

    fix(Header): Typo

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -6,7 +6,7 @@
         <option value="" disabled>Общежития</option>
         <option v-for="option in options" :key="option">{{ option }}</option>
       </select>
-      <div class="header__info_support">Теходдержка</div>
+      <div class="header__info_support">Техподдержка</div>
     </div>
     <router-link class="header__auth" to="/sign-up">Регистрация</router-link>
   </section>
